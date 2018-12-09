67424827057088b4665ec75a9ebbd8f3d47996fe
Alec Adub <adub.alec@gmail.com>
2018-12-08 19:01:01 -0500

    Footer

--- frontend/loginInfo.php
+++ frontend/loginInfo.php
@@ -9,8 +9,11 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
           integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
     <!-- LoginInfo CSS -->
     <link rel="stylesheet" type="text/css" href="style/css/loginInfo.css">
+
+
 
     <title>P4P Login Page</title>
 </head>
@@ -40,12 +43,12 @@
 <div class="container ml-5 mr-5 mx-auto ">
     <div class="row">
         <div class="col-sm-6">
-            <div class="card shadow p-3 mb-5 bg-white rounded">
+            <div class="card shadow p-3 mb-5 rounded ">
                 <div class="card-body">
                     <h5 class="card-title">Task for the semester</h5>
                     <p class="card-text">
                     <script>
-                        for (var i = 0; i < 6; i++) {
+                        for (let i = 0; i < 6; i++) {
                             document.write("<div class=\"input-group mb-3\">\n" +
                                 "                        <div class=\"input-group-prepend\">\n" +
                                 "                            <div class=\"input-group-text\">\n" +
@@ -76,17 +79,143 @@
     </div>
 </div>
 
-<!-- Print Checkbox and text form -->
-<script>
-    function printCheckboxes() {
-        var i;
-        var text = "";
-        for (i = 0; i < 5; i++) {
-            text += "hollla" + "<br>"
-        }
-        return text;
-    }
-</script>
+<div class="blackBackground">
+    <!-- Footer -->
+    <footer class="page-footer font-small">
+
+        <div style="background-color: #007BFF;">
+            <div class="container">
+
+                <!-- Grid row-->
+                <div class="row py-4 d-flex align-items-center">
+
+                    <!-- Grid column -->
+                    <div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
+                        <h6 class="mb-0 white">Get connected with us on social networks!</h6>
+                    </div>
+                    <!-- Grid column -->
+
+                    <!-- Grid column -->
+                    <div class="col-md-6 col-lg-7 text-center text-md-right">
+
+                        <!-- Facebook -->
+                        <a class="fb-ic">
+                            <i class="fa fa-facebook white-text mr-4 white"> </i>
+                        </a>
+                        <!-- Twitter -->
+                        <a class="tw-ic">
+                            <i class="fa fa-twitter white-text mr-4 white"> </i>
+                        </a>
+                        <!-- Google +-->
+                        <a class="gplus-ic">
+                            <i class="fa fa-google-plus white-text mr-4 white"> </i>
+                        </a>
+                        <!--Linkedin -->
+                        <a class="li-ic">
+                            <i class="fa fa-linkedin white-text mr-4 white"> </i>
+                        </a>
+                        <!--Instagram-->
+                        <a class="ins-ic">
+                            <i class="fa fa-instagram white-text white"> </i>
+                        </a>
+
+                    </div>
+                    <!-- Grid column -->
+
+                </div>
+                <!-- Grid row-->
+
+            </div>
+        </div>
+
+        <!-- Footer Links -->
+        <div class="container text-center text-md-left mt-5">
+
+            <!-- Grid row -->
+            <div class="row mt-3">
+
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+
+                    <!-- Content -->
+                    <h6 class="text-uppercase font-weight-bold white">P4P</h6>
+                    <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                    <p class="white">P4P is a web app helping students during their exam period.</p>
+
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+
+                    <!-- Links -->
+                    <h6 class="text-uppercase font-weight-bold"><span class="white">Products</span> </h6>
+                    <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                    <p>
+                        <a href="#!">P1P</a>
+                    </p>
+                    <p>
+                        <a href="#!">P2P</a>
+                    </p>
+                    <p>
+                        <a href="#!">P3P</a>
+                    </p>
+                    <p>
+                        <a href="#!">P4P</a>
+                    </p>
+
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+
+                    <!-- Links -->
+                    <h6 class="text-uppercase font-weight-bold"><span class="white">Useful links</span></h6>
+                    <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                    <p>
+                        <a href="#!">Class #1</a>
+                    </p>
+                    <p>
+                        <a href="#!">Class #2</a>
+                    </p>
+                    <p>
+                        <a href="#!">Class #3</a>
+                    </p>
+                    <p>
+                        <a href="#!">Help</a>
+                    </p>
+
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+
+                    <!-- Links -->
+                    <h6 class="text-uppercase font-weight-bold"><span class="white">Contact</span></h6>
+                    <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                    <p>
+                        <i class="fa fa-home mr-3" style="color: #f1f1f1"></i><span class="white">North Pole</span></p>
+                    <p>
+                        <i class="fa fa-envelope mr-3" style="color: #f1f1f1"></i> <span class="white">nicolasnoel@northpole.np</span> </p>
+                    <p>
+                        <i class="fa fa-phone mr-3" style="color: #f1f1f1"></i> <span class="white">01 234 567 88</span></p>
+                    <p>
+                        <i class="fa fa-print mr-3" style="color: #f1f1f1"></i> <span class="white">01 234 567 89</span></p>
+
+                </div>
+                <!-- Grid column -->
+
+            </div>
+            <!-- Grid row -->
+
+        </div>
+        <!-- Footer Links -->
+
+    </footer>
+    <!-- Footer -->
+</div>
 
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
